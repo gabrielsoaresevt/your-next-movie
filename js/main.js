269badb9a647dc3f6fcb1a007bdc40f599368327
GabrielEVT <gabrielsoaresevt@gmail.com>
2023-02-10 21:16:50 -0300

    fix: get api url

--- js/main.js
+++ js/main.js
@@ -4,12 +4,14 @@ import {
   LANGUAGE,
 } from './api.js'
 
+const API_KEY = config.API_KEY;
+
 window.addEventListener("load", () => {
   const button = document.querySelector('.search-movie-button');
   button.addEventListener('click', getRandomMovie);
   function getRandomMovie() {
     const MOVIE = (Math.round(Math.random() * (1000 - 1) + 1));
-    const apiUrl = BASE_URL+MOVIE+config.API_KEY+LANGUAGE;  
+    const apiUrl = BASE_URL+MOVIE+API_KEY+LANGUAGE;  
     fetch(apiUrl)
       .then(response => response.json())
       .then(itemMovie => {
